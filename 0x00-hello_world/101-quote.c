f67fd2828d4fbe0a15ab5e27c5d1446da804ae9f
medelmir <medelmir001@gmail.com>
2023-03-16 12:01:43 -0700

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,9 @@
-/**
-        ELMIR MED
- *
- */
 #include <stdio.h>
 #include <unistd.h>
 /**
  * main - Entry point
  *
- * Return: 1 (Success)
+ * Return: 1 (!Success)
  */
 int main(void)
 {write(0, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 48);
