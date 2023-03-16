fde19540698a319ad300f8ec39c0ef17a88c2079
medelmir <medelmir001@gmail.com>
2023-03-16 03:30:50 -0700

    c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * "Programming is like building a multilingual puzzle
+ *
+ * Med Elmir
+ */
+int main(void)
+{puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
