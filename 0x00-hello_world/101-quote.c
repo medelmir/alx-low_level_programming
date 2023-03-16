01d5228cd12e9bdd33501f5bb2bc84c39d997c83
medelmir <medelmir001@gmail.com>
2023-03-16 12:10:10 -0700

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,11 +6,7 @@
  * Return: 1 (Success)
  */
 int main(void)
-int main(void)
 {
-	write(2,
-	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	      59);
-
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);	
+return (1);
 }
