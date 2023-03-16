28bd80b38c6e455677bd8d23b2b1c85c64ff3a0d
medelmir <medelmir001@gmail.com>
2023-03-16 03:38:16 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * "Programming is like building a multilingual puzzle
+ * main - Entry point
  *
- * Med Elmir
+ * Return: Always 0 (Success)
  */
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
-return 0;
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
