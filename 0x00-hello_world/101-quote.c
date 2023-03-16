bd0344ddacae6ec19d00b978c8d19a28ad51baa9
medelmir <medelmir001@gmail.com>
2023-03-16 12:08:41 -0700

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
+ * main - A c program that prints a line to the standard error
  *
- * Return: 1 (!Success)
+ * Return: 1 (Success)
  */
 int main(void)
-{write(0, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 48);
-return (1);
-}
+int main(void)
+{
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
 
+	return (1);
+}
