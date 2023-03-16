fed180159e3f8e030c3fd83b4d27ae06966830b3
medelmir <medelmir001@gmail.com>
2023-03-16 12:03:07 -0700

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  * Return: 1 (!Success)
  */
 int main(void)
-{write(0, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 48);
+{write(0, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 48);
 return (1);
 }
 
