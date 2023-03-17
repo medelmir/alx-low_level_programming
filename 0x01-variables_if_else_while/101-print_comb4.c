cb165185ecc21611800c0c2a80dbf9d38d5f96c5
medelmir <medelmir001@gmail.com>
2023-03-17 15:21:50 -0700

    c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,16 +6,16 @@
 int main(void)
 {
 int i, j, k;
-for (i = 0 ; i <= 7 ; i++) 
+for (i = 0 ; i <= 7 ; i++)
 {
-for (j = i + 1 ; j <= 8 ; j++) 
+for (j = i + 1 ; j <= 8 ; j++)
 {
-for (k = j + 1 ; k <= 9 ; k++) 
+for (k = j + 1 ; k <= 9 ; k++)
 {
 putchar('0' + i);
 putchar('0' + j);
 putchar('0' + k);
-if (i != 7 || j != 8 || k != 9) 
+if (i != 7 || j != 8 || k != 9)
 {
 putchar(',');
 putchar(' ');
@@ -25,4 +25,3 @@ putchar(' ');
 }
 putchar('\n');
 return (0);
-}
