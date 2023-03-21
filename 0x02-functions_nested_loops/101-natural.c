2e485901326285d30f5ea7c94e8625d3cd2d8c68
medelmir <medelmir001@gmail.com>
2023-03-21 11:51:46 -0700

    c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,14 +6,14 @@
  * 5 below 1024 (excluded), followed by a new line
  * Return: 0
  */
-int main()
+int main(void)
 {
 int sum, i;
 for (i = 0 ; i < 1024 ; i++)
 {
 if (i % 3 == 0 || i % 5 == 0)
 {
-sum = sum + i ;
+sum = sum + i;
 }
 }
 printf("%d\n", sum);
