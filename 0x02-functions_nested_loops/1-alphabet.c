5c5fd4ba61f47c59adb088d660a9d59425e6c1f7
medelmir <medelmir001@gmail.com>
2023-03-21 02:43:31 -0700

    c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* main : 'prints the alphabet, in lowercase, followed by a new line'
+*
+*
+*/
+void print_alphabet()
+{
+char n;
+for (n='a' ;  n <= 'z' ; n++)
+{
+_putchar(n);
+}
+_putchar('\n');
+}
+
