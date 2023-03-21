ef71ee535c9a7436c8b62935fcb9b8d694d31b0d
medelmir <medelmir001@gmail.com>
2023-03-21 03:10:14 -0700

    c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 /**
 * print_sign - prints the sign of a number
 * @n: number
-* Return: 1 and prints + if n is greater than zero , 0 and prints 0 if n is zero , -1 and prints - if n is less than zero
+* Return: 1 if n>zero,0 if n== zero,-1 if n< zero
 */
 int print_sign(int n)
 {
