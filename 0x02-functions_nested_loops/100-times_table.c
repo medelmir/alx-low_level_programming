c8d17521f0837ca1b4178640f968aa6c22ea0154
medelmir <medelmir001@gmail.com>
2023-03-21 06:04:17 -0700

    c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,30 +15,30 @@ for (j = 0; j <= n; j++)
 k = j * i;
 if (j == 0)
 {
-putchar (k + '0');
+_putchar (k + '0');
 }
 if (k < 10 && j != 0)
 {
-putchar (',');
-putchar (' ');
-putchar (' ');
-putchar (k + '0');
+_putchar (',');
+_putchar (' ');
+_putchar (' ');
+_putchar (k + '0');
 }
 else if (k >= 10 && k < 100)
 {
-putchar (',');
-putchar (' ');
-putchar ((k / 10) + '0');
-putchar ((k % 10) + '0');
+_putchar (',');
+_putchar (' ');
+_putchar ((k / 10) + '0');
+_putchar ((k % 10) + '0');
 }
 else if (k >= 100)
 {
-putchar(',');
-putchar(' ');
-putchar((k / 100) + '0');
-putchar(((k / 10) % 10) + '0');
-putchar((k % 10) + '0');
+_putchar(',');
+_putchar(' ');
+_putchar((k / 100) + '0');
+_putchar(((k / 10) % 10) + '0');
+_putchar((k % 10) + '0');
 }
 }
-putchar ('\n');
+_putchar ('\n');
 }
