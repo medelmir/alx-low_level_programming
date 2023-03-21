bd046756b2910251ca223ebadd1d5bbe11167bea
medelmir <medelmir001@gmail.com>
2023-03-21 03:24:08 -0700

    c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: the int to check
+ * Return: the last digit
+ */
+int print_last_digit(int n)
+{
+int l;
+l =  n % 10;
+if (l < 0)
+{
+_putchar (-l + 48);
+return (-l);
+}
+else
+{
+_putchar (l + 48);
+return (l);
+}
+}
