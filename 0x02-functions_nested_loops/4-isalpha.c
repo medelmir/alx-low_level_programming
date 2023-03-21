4ec30f44e6437e5cd3417544221bdd612e1d0c47
medelmir <medelmir001@gmail.com>
2023-03-21 03:02:58 -0700

    c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+include "main.h"
+/**
+* _isalpha - checks for alphabetic character
+*  @c: character
+*  Return: 1 if c is a letter , 0 otherwise
+*/
+int _isalpha(int c)
+{
+if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+_putchar ('\n');
+}
