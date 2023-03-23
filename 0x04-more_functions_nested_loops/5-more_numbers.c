a40ddf8c4914a98989a94e16c3fafa9f494fe86a
medelmir <medelmir001@gmail.com>
2023-03-23 04:28:47 -0700

    c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,14 +6,14 @@
 void more_numbers(void)
 {
 int i, j;
-char ch;    
+char ch;
 for (i = 1; i <= 10; i++)
 {
 for (j = 0; j <= 14; j++)
 {
 if (j >= 10)
 {
-ch = '1';  
+ch = '1';
 _putchar(ch);
 }
 ch = j % 10 + '0';
