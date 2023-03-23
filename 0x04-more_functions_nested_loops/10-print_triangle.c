4cefd8f5940bed915052b215e545da5ce10e51ee
medelmir <medelmir001@gmail.com>
2023-03-23 13:50:58 -0700

    c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,17 +10,17 @@ void print_triangle(int size)
 int i;
 int j;
 int k;
-if(size <= 0)
+if (size <= 0)
 {
 _putchar('\n');
 }
-for(i = 1; i <= size; i++)
+for (i = 1; i <= size; i++)
 {
-for(j = 1; j <= size - i; j++)
+for (j = 1; j <= size - i; j++)
 {
 _putchar(' ');
 }
-for(k = 1; k <= i; k++)
+for (k = 1; k <= i; k++)
 {
 _putchar('#');
 }
