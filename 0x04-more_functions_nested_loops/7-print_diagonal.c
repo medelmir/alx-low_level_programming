f048a290d41506bb8c1df76b670f474b01ba2b06
medelmir <medelmir001@gmail.com>
2023-03-23 10:57:12 -0700

    c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,20 +7,15 @@ void print_diagonal(int n)
 {
 if (n <= 0)
 {
-_putchar('\n');
-} else
-{
-int i, j;
-for (i = 0; i < n; i++)
-{
-for (j = 0; j < n; j++)
-{
-if (j == i)
-_putchar('\\');
-else if (j < i)
-_putchar(' ');
+putchar('\n');
 }
-_putchar('\n');
+for (int i = 0; i < n; i++)
+{
+for (int j = 0; j < i; j++)
+{
+putchar(' ');
 }
+putchar('\\');
+putchar('\n');
 }
 }
