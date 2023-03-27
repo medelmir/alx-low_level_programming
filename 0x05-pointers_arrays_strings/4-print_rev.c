41834417ad708856bffc7e4af44cb09db78d0a2b
medelmir <medelmir001@gmail.com>
2023-03-27 06:54:04 -0700

    c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* print_rev - prints a string, in reverse, followed by a new line.
+* @s : pointer
+*
+*
+*/
+void print_rev(char *s)
+{
+int len = 0;
+int i;
+while (s[len] != '\0')
+{
+len++;
+}
+
+for (i = len - 1; i >= 0; i--)
+{
+putchar(s[i]);
+}
+putchar('\n');
+}
