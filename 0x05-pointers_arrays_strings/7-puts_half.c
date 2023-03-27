f7012ee1ba5d0772f06a68f74ec8a4c48b0752a8
medelmir <medelmir001@gmail.com>
2023-03-27 08:31:53 -0700

    c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,13 +7,22 @@
 */
 void puts_half(char *str)
 {
+int i;
 int len = 0;
 while (str[len] != '\0')
 {
 len++;
 }
-int n = (len % 2 == 0) ? len / 2 : (len - 1) / 2;
-for (int i = n; i < len; i++)
+int n;
+if (len % 2 == 0) 
+{
+n = len / 2;
+}
+else
+{
+n = (len - 1) / 2;
+}
+for (i = n; i < len; i++)
 {
 _putchar(str[i]);
 }
