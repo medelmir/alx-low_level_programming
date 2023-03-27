ceb0517556478f0be4e0b399897e9b9d8e938c83
medelmir <medelmir001@gmail.com>
2023-03-27 15:08:51 -0700

    c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - program that generates random valid
+* passwords for the program 101-crackme
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+srand(time(NULL));
+int sum = 0;
+int last_digit;
+int i;
+for (i = 0; i < 100; i++)
+{
+int digit = rand() % 10;
+sum += digit;
+putchar(digit + '0');
+}
+last_digit = (2772 - sum) % 10;
+putchar(last_digit + '0');
+return (0);
+}
