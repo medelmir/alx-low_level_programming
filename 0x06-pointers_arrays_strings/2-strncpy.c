5e4b42ac4948ed4abea4bc04bdc0fe2b3198f190
medelmir <medelmir001@gmail.com>
2023-03-29 09:36:49 -0700

    c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * *  _strncpy - copy a string
- * *  @dest: input value
- * *  @src: input value
- * *  @n: input value
- * *  Return: dest
- * */
+*  _strncpy - copy a string
+*  @dest: input value
+*  @src: input value
+*  @n: input value
+*  Return: dest
+*/
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;
