45801c5fcbb310d45e697355c7e8b322b52f5bb3
medelmir <medelmir001@gmail.com>
2023-03-29 11:17:46 -0700

    c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* reverse_array - reverses the content of an array of integers
+* @a : array
+* @n : param
+*/
+void reverse_array(int *a, int n)
+{
+int i, j, temp;
+for (i = 0, j = n - 1; i < j; i++, j--)
+{
+temp = a[i];
+a[i] = a[j];
+a[j] = temp;
+}
+}
