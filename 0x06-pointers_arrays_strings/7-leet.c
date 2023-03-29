4ecff6faf23af9c1f8bba90006d8d23db4d3e696
medelmir <medelmir001@gmail.com>
2023-03-29 14:36:00 -0700

    c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,19 +6,18 @@
 */
 char *leet(char *src)
 {
-int len = 0;
-int i;
-while (src[len] != '\0')
+int i, j;
+char s1[] = "aAeEoOtTlL";
+char s2[] = "4433007711";
+for (i = 0; src[i] != '\0'; i++)
 {
-len++;
+for (j = 0; j < 10; j++)
+{
+if (src[i] == s1[j])
+{
+src[i] = s2[j];
 }
-for (i = 0 ; i < len ; i++)
-{
-src[i] = (src[i] == 'a' || src[i] == 'A') ? '4' : src[i];
-src[i] = (src[i] == 'e' || src[i] == 'E') ? '3' : src[i];
-src[i] = (src[i] == 'o' || src[i] == 'O') ? '0' : src[i];
-src[i] = (src[i] == 't' || src[i] == 'T') ? '7' : src[i];
-src[i] = (src[i] == 'l' || src[i] == 'L') ? '1' : src[i];
+}
 }
 return (src);
 }
