3042a3890aa1932ef04bd3dab362ec46d20bd34e
medelmir <medelmir001@gmail.com>
2023-03-29 17:46:10 -0700

    c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,12 +9,12 @@ void print_number(int n)
 {
 if (n < 0)
 {
-putchar('-');
+_putchar('-');
 n = -n;
 }
 if (n / 10 != 0)
 {
 print_number(n / 10);
 }
-putchar(n % 10 + '0');
+_putchar(n % 10 + '0');
 }
