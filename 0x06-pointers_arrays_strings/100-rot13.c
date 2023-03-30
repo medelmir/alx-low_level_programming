5e8d561ef5fffb7251731329f918c408a7ada7b2
medelmir <medelmir001@gmail.com>
2023-03-29 17:15:02 -0700

    c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,19 +8,19 @@
 char *rot13(char *result)
 {
 int i;
+int j;
 char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-while (*result)
+for (j = 0 ; result[i] != '\0' ; i++)
 {
 for (i = 0; i < 52; i++)
 {
-if (*result == rot13[i])
+if (result[j] == rot13[i])
 {
-*result = ROT13[i];
+result[j]  = ROT13[i];
 break;
 }
 }
-result++;
 }
 return (result);
 }
