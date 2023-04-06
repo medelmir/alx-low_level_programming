9c92ed2e420a916974ebbfece87675d143d6837b
medelmir <medelmir001@gmail.com>
2023-04-06 16:36:18 -0700

    c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - program that multiplies two numbers
+* @argc: int
+* @argv: list
+* Return: 1 if argc < 2 , otherwise 0
+*/
+int main(int argc, char *argv[])
+{
+        int i;
+        int k;
+        int z;
+        (void)*argv;
+        if (argc - 1 != 2)
+        {
+                printf("Error\n");
+                return (1);
+        }
+        else
+        {
+                i = atoi(argv[1]);
+                k = atoi(argv[2]);
+                z = i * k;
+                printf("%d\n", z);
+        }
+        return (0);
+}
