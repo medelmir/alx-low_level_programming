557128a7adebc80c77360a5441a8928197d2a0f4
medelmir <medelmir001@gmail.com>
2023-04-07 14:09:00 -0700

    c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,9 +7,11 @@
 */
 int main(int argc, char *argv[])
 {
-        int i;
-        (void)argc;
-        for (i = 0; i < argc ; i++)
-                printf("%s\n", argv[i]);
-        return (0);
+int i;
+(void)argc;
+for (i = 0; i < argc ; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
 }
