ba48c9b10679c53da3267f91ebaf79c4959aa68d
medelmir <medelmir001@gmail.com>
2023-04-07 13:58:33 -0700

    c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,11 +4,10 @@
 * @argc: int
 * @argv: list
 * Return: 0
-*
 */
 int main(int argc, char *argv[])
 {
-        printf("%s\n", argv[0]);
-        (void)argc;
-        return (0);
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
 }
