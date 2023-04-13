fe475a3c3c6d4abd7e43d526e375242a296a4a91
medelmir <medelmir001@gmail.com>
2023-04-13 14:54:44 -0700

    c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* malloc_checked - function that allocates memory
+* @b: param
+* Return: 0
+*/
+void *malloc_checked(unsigned int b)
+{
+void *p
+p = malloc(b);
+if (p == NULL)
+{
+exit(98);
+}
+return (p);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,18 @@
+C - More malloc, free
+
+TASKS : 
+
+Trust no one
+
+
+string_nconcat
+
+calloc
+
+
+array_range
+
+realloc
+
+
+We must accept finite disappointment, but never lose infinite hope
