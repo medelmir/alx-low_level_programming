6f60a6e4a40bdf534705655a50f4e3e5ca1e9319
medelmir <medelmir001@gmail.com>
2023-04-17 14:07:21 -0700

    c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,9 +11,9 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+dog_t *dog;
 if (!name || !owner || age < 0)
 return (NULL);
-dog_t *dog;
 dog = malloc(sizeof(dog_t));
 if (dog == NULL)
 return (NULL);
