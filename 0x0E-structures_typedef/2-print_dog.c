53cd7fbd83e92e60fde7760a722a0ff937c83356
medelmir <medelmir001@gmail.com>
2023-04-17 11:41:58 -0700

    c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,7 +16,7 @@ printf("Name: %s\n", d->name);
 if (d->age == NULL)
 printf("Age: (nil)\n");
 else
-printf("Age: %s\n", d->age);
+printf("Age: %f\n", d->age);
 if (d->owner == NULL)
 printf("Owner: (nil)\n");
 else
