3985905422873a9cf7d78395034ce59116a1f6a8
medelmir <medelmir001@gmail.com>
2023-04-17 11:40:06 -0700

    c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,15 +10,15 @@ void print_dog(struct dog *d)
 if (d)
 {
 if (d->name == NULL)
-printf("Name: (nil)\n")
+printf("Name: (nil)\n");
 else
 printf("Name: %s\n", d->name);
 if (d->age == NULL)
-printf("Age: (nil)\n")
+printf("Age: (nil)\n");
 else
 printf("Age: %s\n", d->age);
 if (d->owner == NULL)
-printf("Owner: (nil)\n")
+printf("Owner: (nil)\n");
 else
 printf("Owner: %s\n", d->owner);
 }
