e0a9f4cab75dd32d90d0e13f1e53049b849d01d4
medelmir <medelmir001@gmail.com>
2023-04-17 16:18:04 -0700

    c

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef _OBJECT_LIKE_MACRO.H
+#define _OBJECT_LIKE_MACRO.H
+#define SIZE 1024
+#endif
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,6 @@
+Tasks
+0. Object-like Macro
+1. Pi
+2. File name
+3. Function-like macro
+4. SUM
