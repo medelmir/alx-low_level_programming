0898f84ec354f9ae450924f01407052ab900a289
medelmir <medelmir001@gmail.com>
2023-04-19 18:33:08 -0700

    c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - check the code for The School students.
+ * @argc: the number of arguments
+ * @argv: argument vector
+ * Return: 0
+*/
+int main(int argc, char **argv)
+{
+char *p = (char *)main;
+int b;
+if (argc != 2)
+printf("Error\n"), exit(1);
+b = atoi(argv[1]);
+if (b < 0)
+printf("Error\n"), exit(2);
+while (b--)
+printf("%02hhx%s", *p++, b ? " " : "\n");
+return (0);
+}
