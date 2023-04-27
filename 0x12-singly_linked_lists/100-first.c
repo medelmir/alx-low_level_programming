c16473dbc64ed47d8f370c8e99c8b8f7f9f887f5
medelmir <medelmir001@gmail.com>
2023-04-27 12:11:37 -0700

    c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,11 @@
 #include "lists.h"
-/*
+/**
 * _constructor - prints a string before the main.
 * Return: void
 */
 void _constructor(void)
 {
-printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,
+\nI bore my house upon my back!\n");
 }
 
