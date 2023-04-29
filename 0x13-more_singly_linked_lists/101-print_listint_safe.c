c8caa9bf23a5afaf3b72af59a6d66550d5b95853
medelmir <medelmir001@gmail.com>
2023-04-29 08:12:06 -0700

    update

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -5,15 +5,22 @@
  * Return: the number of nodes in the list
 */
 size_t print_listint_safe(const listint_t *head)
-{    
+{
 size_t i = 0;
+const listint_t *temporary1 = head;
+const listint_t *temporary2 = head;
 if (head == NULL)
 exit(98);
-while (head != NULL)
+while (temporary1 != NULL)
 {
-printf("[%p] %d\n", (void *)head, head->n);
-head = head->next;
+printf("[%p] %d\n", (void *)temporary1, temporary1->n);
 i++;
+temporary1 = temporary1->next;
+if (temporary1 >= temporary2)
+{
+printf("-> [%p] %d\n", (void *)temporary1, temporary1->n);
+break;
+}
 }
 return (i);
 }
