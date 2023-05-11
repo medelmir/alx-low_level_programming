2411fdc73b2a88457b9d3f3706b6534dd8bbea83
medelmir <medelmir001@gmail.com>
2023-05-11 13:03:31 -0700

    c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
 int create_file(const char *filename, char *text_content)
 {
 int filed, i, wr;
-if (filename == NULL)
+if (!filename)
 return (-1);
 filed = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 for (i = 0; text_content[i] != '\0'; i++)
