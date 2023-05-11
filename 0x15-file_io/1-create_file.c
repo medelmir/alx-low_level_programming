9198efb1317736bb008bc6c31fb2f93f21c0280c
medelmir <medelmir001@gmail.com>
2023-05-11 12:56:39 -0700

    c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: name of file to create
+ * @text_content: text to write to file
+ * Return: 1 on success, -1 on failure
+*/
+int create_file(const char *filename, char *text_content)
+{
+int filed, i, wr;
+if (filename == NULL)
+return (-1);
+filed = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+for (i = 0; text_content[i] != '\0'; i++)
+;
+if (filed == -1)
+return (-1);
+if (text_content == NULL)
+return (1);
+wr = write(filed, text_content, i);
+if (wr == -1)
+return (-1);
+close(filed);
+return (1);
+}
+
