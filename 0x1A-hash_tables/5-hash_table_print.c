a4e719cf79bf56f213e3a5979ad09eb61626afdf
medelmir <medelmir001@gmail.com>
2023-07-07 03:21:05 -0700

    c

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_table.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+unsigned long int i;
+hash_node_t *node;
+int flag = 0;
+if (ht == NULL)
+return;
+printf("{");
+for (i = 0; i < ht->size; i++)
+{
+node = ht->array[i];
+while (node != NULL)
+{
+if (flag == 1)
+printf(", ");
+printf("'%s': '%s'", node->key, node->value);
+flag = 1;
+node = node->next;
+}
+}
+printf("}\n");
+}
